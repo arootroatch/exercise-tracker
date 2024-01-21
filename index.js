aab4a7427591af46afc5cff4148b69dd3e15a47b
arootroatch <65352758+arootroatch@users.noreply.github.com>
2024-01-21 00:50:10 +0000

    initial commit to Github-project finished

--- index.js
+++ index.js
@@ -1,14 +1,125 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+mongoose.connect(process.env.MONGO_URI);
+
+const UserSchema = new Schema({
+  username: {type: String, required: true},
+});
+const User = mongoose.model('User', UserSchema);
+
+const ExerciseSchema = new Schema({
+  user_Id: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, required: true},
+});
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', async (req, res)=>{
+  const userObj = new User({username: req.body.username});
+
+  try{
+    const user = await userObj.save();
+    res.json(user);
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post('/api/users/:_id/exercises', async (req, res)=>{
+  const id = req.params._id;
+  const {description, duration, date} = req.body;
+  // console.log('POST',req.body, req.params);
+
+  try{
+    const user = await User.findById(id);
+    if(!user){
+      res.send("Could not find user");
+    } else {
+      const exerciseObj = new Exercise({
+        user_Id: user._id,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date(),
+      });
+      const exercise = await exerciseObj.save();
+      
+      res.json({
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString(),
+        _id: user._id,
+      })
+    }
+  } catch(err){
+    console.log(err);
+    res.send("There was an error saving the exercise");
+  }
+})
+
+app.get('/api/users', async (req, res)=>{
+  const users = await User.find().select('username _id');
+  if (!users){
+    res.send("No users");
+  } else {
+    res.send(users);
+  }
+});
+
+app.get('/api/users/:_id/logs', async (req, res)=>{
+  const {from, to, limit} = req.query;
+  const id = req.params._id;
+  const user = await User.findById(id);
+  if(!user){
+    res.send("Could not find user");
+    return;
+  } 
+  let dateObj = {};
+  if(from){
+    dateObj['$gte'] = new Date(from);
+  }
+  if(to){
+    dateObj['$lte'] = new Date(to);
+  }
+  let filter = {
+    user_Id: id
+  }
+  if(from||to){
+    filter.date = dateObj;
+  }
+  const exercises = await Exercise.find(filter).limit(+limit??500);
+  const log = exercises.map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString(),
+  }));
+
+  console.log('GET',req.body, req.query, {
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log,
+  });
+  res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log,
+  })
+});
 
 
 
